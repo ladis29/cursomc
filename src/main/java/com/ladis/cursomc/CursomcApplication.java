dcfeb71a451f90c45004ea6f0485627ba73b0c52
Ladislau Felisbino <ladis29@gmail.com>
2018-10-16 17:25:38 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/ladis/cursomc/CursomcApplication.java
+++ src/main/java/com/ladis/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.ladis.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.ladis.cursomc.domain.Cidade;
 import com.ladis.cursomc.domain.Cliente;
 import com.ladis.cursomc.domain.Endereco;
 import com.ladis.cursomc.domain.Estado;
+import com.ladis.cursomc.domain.Pagamento;
+import com.ladis.cursomc.domain.PagamentoComBoleto;
+import com.ladis.cursomc.domain.PagamentoComCartao;
+import com.ladis.cursomc.domain.Pedido;
 import com.ladis.cursomc.domain.Produto;
+import com.ladis.cursomc.domain.enums.EstadoPagamento;
 import com.ladis.cursomc.domain.enums.TipoCliente;
 import com.ladis.cursomc.repositories.CategoriaRepository;
 import com.ladis.cursomc.repositories.CidadeRepository;
 import com.ladis.cursomc.repositories.ClienteRepository;
 import com.ladis.cursomc.repositories.EnderecoRepository;
 import com.ladis.cursomc.repositories.EstadoRepository;
+import com.ladis.cursomc.repositories.PagamentoRepository;
+import com.ladis.cursomc.repositories.PedidoRepository;
 import com.ladis.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -26,21 +34,20 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -96,6 +103,24 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		//Instanciação de Pedidos, pagamentos e Estado de Pagamentos
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+				
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 }
--- src/main/java/com/ladis/cursomc/domain/Cliente.java
+++ src/main/java/com/ladis/cursomc/domain/Cliente.java
@@ -19,7 +19,6 @@ import com.ladis.cursomc.domain.enums.TipoCliente;
 
 @Entity
 public class Cliente implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -37,6 +36,10 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones =  new HashSet<>();
+	
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 		
@@ -106,6 +109,14 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -131,7 +142,7 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
+		
 
 }
